1ac624bc31115de6812cdabf92ca05d16d14735a
Ian Dennis Miller <ian@iandennismiller.com>
2016-04-30 13:25:11 -0400

    groundwork for installing project apps

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+###
 # update system
+###
 
 apt-get update
 apt-get -y upgrade
+
+###
+# install essential services
+###
 
 # nginx
 
@@ -21,3 +27,16 @@ apt-get install -y postgresql
 # sudo su - postgres
 # psql -c "CREATE ROLE rss_aggregator PASSWORD 'aaa' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN"
 # psql -c "CREATE DATABASE rss_aggregator OWNER rss_aggregator"
+
+###
+# install pubgem processes
+###
+
+# rss-aggregator
+
+# create rss-aggregator user
+# git clone "https://github.com/pubgem/rss-aggregator.git"
+
+# bib-curator
+
+# front-page
