8d837d4ef0dc491c165b0200c701a4fdcd8aecc8
Ian Dennis Miller <ian@iandennismiller.com>
2016-05-02 09:20:46 -0400

    port forwarding for rss-aggregator, app installation

--- applications.sh
+++ applications.sh
@@ -4,17 +4,22 @@
 # rss-aggregator
 ###
 
-# create rss-aggregator user
+# user: rss-aggregator
 useradd --system --user-group --create-home rss-aggregator
 
-cd /home/rss-aggregator
-sudo -u rss-aggregator git clone "https://github.com/pubgem/rss-aggregator.git"
-
-cd /home/rss-aggregator/rss-aggregator
-# sudo -u rss-aggregator 'source /usr/local/bin/virtualenvwrapper.sh && mkvirtualenv -a /home/rss-aggregator/rss-aggregator rss-aggregator'
-
+# database: rss-aggregator
 sudo -u postgres psql -c "CREATE ROLE rss_aggregator PASSWORD 'aaa' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN"
 sudo -u postgres psql -c "CREATE DATABASE rss_aggregator OWNER rss_aggregator"
+
+# install: rss-aggregator
+exec sudo -i -u rss-aggregator /bin/bash - << eof
+    source "/home/rss-aggregator/.bashrc"
+    git clone "https://github.com/pubgem/rss-aggregator.git"
+    cd rss-aggregator
+    mkvirtualenv -a ~/rss-aggregator rss-aggregator
+    workon rss-aggregator
+    make install db test
+eof
 
 ###
 # citation-curator
